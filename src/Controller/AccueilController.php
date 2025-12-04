82a4d0b32ed5c23230b6c62af37213927b33bc3e
Aereises <ruthmetayer0@gmail.com>
2025-12-05 00:22:38 +0100

    feat ajout d'une page d'acceuil ()

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,23 @@
+<?php
+/*
+    * Projet Nuit de l’Info — 2025
+* Ce programme est publié sous licence GNU AGPLv3.
+* Vous pouvez obtenir une copie de la licence à :
+* https://www.gnu.org/licenses/agpl-3.0.html
+*/
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AccueilController extends AbstractController
+{
+    #[Route('/', name: 'app_accueil')]
+    public function index(): Response
+    {
+        return $this->render('accueil/accueil.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+}
